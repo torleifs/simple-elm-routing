4aef6c9bfa131bb94a6e49bd38228ef3fda11f4a
Torleif Sandnes <tsandnes@akvagroup.com>
2020-02-03 17:44:56 +0100

    Added navbar

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+import Bootstrap.CDN as CDN
 import Bootstrap.Navbar as Navbar
 import Browser
 import Browser.Navigation as Nav
@@ -16,18 +17,33 @@ type Msg
     = Dummy
     | LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
+    | NavbarMsg Navbar.State
+
+
+
+-- Model
 
 
 type alias Model =
     { currentPage : Page
+    , navbarState : Navbar.State
     }
+
+
+
+-- The navbar needs to know the initial window size, so the inital state for a navbar requires a command to be run by the Elm runtime
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
+    let
+        ( navbarState, navbarCmd ) =
+            Navbar.initialState NavbarMsg
+    in
     ( { currentPage = InputPage
+      , navbarState = navbarState
       }
-    , Cmd.none
+    , navbarCmd
     )
 
 
@@ -37,7 +53,7 @@ init flags url key =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Navbar.subscriptions model.navbarState NavbarMsg
 
 
 
@@ -47,6 +63,11 @@ subscriptions model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        NavbarMsg state ->
+            ( { model | navbarState = state }
+            , Cmd.none
+            )
+
         _ ->
             ( model, Cmd.none )
 
@@ -56,10 +77,21 @@ update msg model =
 
 
 view : Model -> Browser.Document Msg
-view _ =
+view model =
     { title = "URL Interceptor"
     , body =
-        [ text "The current URL is: "
+        [ CDN.stylesheet
+
+        -- creates an inline style node with the Bootstrap CSS
+        , Navbar.config
+            NavbarMsg
+            |> Navbar.withAnimation
+            |> Navbar.brand [ href "#" ] [ text "Brand" ]
+            |> Navbar.items
+                [ Navbar.itemLink [ href "#" ] [ text "Item 1" ]
+                , Navbar.itemLink [ href "#" ] [ text "Item 2" ]
+                ]
+            |> Navbar.view model.navbarState
         ]
     }
 
